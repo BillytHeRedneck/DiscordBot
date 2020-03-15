3e7f1a9143e698e3f1aa5486b7e230506d71bc88
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-03-15 18:51:53 -0400

    Test

---
+++ bot.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login("Njg4ODQ3ODUwNDY5NDU3OTUy.Xm6TsQ.3kN2pwav_duuU6I-gNz51y6JITQ");
+
+client.once('ready', () => { 
+    console.log('Bot is logged in!')
+})
+
+client.on('message', message => {
+    //console.log(message.content);
+
+    if(message.content.startsWith("!Eric")){
+        message.channel.send({files: ["/Users/billytheredneck/Documents/Other/Discord Gif/Eric Andre Unimpressed.gif"]})
+    } 
+})
+
+client.login(token);
