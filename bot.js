5f50b18adba406855bfd91ec7aa7105ed54c3970
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-03-16 14:57:54 -0400

    Change Help

--- bot.js
+++ bot.js
@@ -12,9 +12,9 @@ client.on('message', message => {
 
     //!Help CODE
     if(message.content.startsWith(prefix + "Help")) {
-        message.channel.send("I am here to help you. Start with my prefix '!', and here are my commands: " + '\n')
-        message.channel.send("Eric" + '\b')
-        message.channel.send("Yesss" + '\b')
+        message.channel.send("I am here to help you. Start with my prefix '!', and here are my commands: ")
+        message.channel.send("Eric" )
+        message.channel.send("Yesss")
 
     //WHERE GIF CODE GOES
     } else if(message.content.startsWith(prefix + "Eric")){
