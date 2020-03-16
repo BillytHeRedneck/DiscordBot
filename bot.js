a4254d83c4134c07bace1d6df8fcd436c676e5ec
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-03-16 12:41:41 -0400

    New Gif

--- bot.js
+++ bot.js
@@ -12,41 +12,15 @@ client.on('message', message => {
 
     if(message.content.startsWith("!Eric")){
         message.channel.send({files : ["./Images/Eric Andre Unimpressed.gif"]})
+    } else if(message.content.startsWith("!Yesss")){
+        message.channel.send({files : ["./Images/Yesss.gif"]})
+    } else if(message.content.startsWith("!EnjoyThat")){
+        message.channel.send({files : ["./Images/EnjoyThat"]})
+    } else if(message.content.startsWith("!CatchHands")){
+        message.channel.send({files : ["./Images/CatchHands.gif"]})
+    } else if(message.content.startsWith("!NOOICE")){
+        message.channel.send({files : ["./Images/NOOICE.gif"]})
     } 
 })
 
 client.login(token);
-/*
-const Discord = require('discord.js');
-const{ prefix, token } = require ('./config.json');
-const client = new Discord.Client();
-//const canvas = canvas.createCanvas(700, 250);
-//const canvas = Canvas.createCanvas(700, 250);
-
-client.on('guildMemberAdd', async member => {
-    const channel = member.guild.channels.find(ch => ch.name === 'member-log');
-    if (!channel) return;
-
-    // Set a new canvas to the dimensions of 700x250 pixels
-    const canvas = Canvas.createCanvas(700, 250);
-    // ctx (context) will be used to modify a lot of the canvas
-
-    const ctx = canvas.getContext('2d');
-
-    channel.send(`Welcome to the server, ${member}!`);
-});
-client.login(token);
-
-const canvas = canvas.createCanvas(700, 250);
-client.once('ready', () => { 
-    console.log('Bot is logged in!')
-})
-
-client.on('message', message => {
-    //console.log(message.content);
-
-    if(message.content.startsWith("!Eric")){
-        message.channel.send({files : ["./Images/Eric Andre Unimpressed.gif"]})
-    } 
-})
-*/
