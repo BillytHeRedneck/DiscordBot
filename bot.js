c1f75711ff0fb071cd636639c7cf045c9abdc628
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-03-15 20:28:43 -0400

    test stay on

--- bot.js
+++ bot.js
@@ -1,16 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const canvas = Canvas.createCanvas(700, 250);
 
 client.login("Njg4ODQ3ODUwNDY5NDU3OTUy.Xm6TsQ.3kN2pwav_duuU6I-gNz51y6JITQ");
 
 client.once('ready', () => { 
     console.log('Bot is logged in!')
 })
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
 
 client.on('message', message => {
     //console.log(message.content);
