ee36cc54ba0bdb7f596452eb78b9c3a6a91a1b23
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-03-15 20:31:54 -0400

    test stay on

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
+const{ prefix, token } = require ('./config.json');
 const client = new Discord.Client();
-const canvas = Canvas.createCanvas(700, 250);
+//const canvas = Canvas.createCanvas(700, 250);
 
 client.login("Njg4ODQ3ODUwNDY5NDU3OTUy.Xm6TsQ.3kN2pwav_duuU6I-gNz51y6JITQ");
 
