4b82c5761637e068590c91db15403447d02f49d2
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-03-15 22:09:44 -0400

    Test

--- bot.js
+++ bot.js
@@ -4,7 +4,21 @@ const client = new Discord.Client();
 //const canvas = canvas.createCanvas(700, 250);
 //const canvas = Canvas.createCanvas(700, 250);
 
+client.on('guildMemberAdd', async member => {
+    const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+    if (!channel) return;
+
+    // Set a new canvas to the dimensions of 700x250 pixels
+    const canvas = Canvas.createCanvas(700, 250);
+    // ctx (context) will be used to modify a lot of the canvas
+
+    const ctx = canvas.getContext('2d');
+
+    channel.send(`Welcome to the server, ${member}!`);
+});
 client.login(token);
+//client.login(token);
+/*
 const canvas = canvas.createCanvas(700, 250);
 client.once('ready', () => { 
     console.log('Bot is logged in!')
@@ -17,5 +31,4 @@ client.on('message', message => {
         message.channel.send({files : ["./Images/Eric Andre Unimpressed.gif"]})
     } 
 })
-
-//client.login(token);
+*/
