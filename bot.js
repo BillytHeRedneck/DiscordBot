f39b94959af4f07d505b014398563362bf180ee8
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-03-16 11:41:47 -0400

    Test

--- bot.js
+++ bot.js
@@ -1,11 +1,21 @@
+
 const Discord = require('discord.js');
+const{ prefix, token } = require ('./config.json');
 const client = new Discord.Client();
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
+client.once('ready', () => { 
+    console.log('Bot is logged in!')
+})
 
-client.login('Njg4ODQ3ODUwNDY5NDU3OTUy.Xm6TsQ.3kN2pwav_duuU6I-gNz51y6JITQ');
+client.on('message', message => {
+    //console.log(message.content);
+
+    if(message.content.startsWith("!Eric")){
+        message.channel.send({files : ["./Images/Eric Andre Unimpressed.gif"]})
+    } 
+})
+
+client.login('token');
 /*
 const Discord = require('discord.js');
 const{ prefix, token } = require ('./config.json');
@@ -26,7 +36,6 @@ client.on('guildMemberAdd', async member => {
     channel.send(`Welcome to the server, ${member}!`);
 });
 client.login(token);
-//client.login(token);
 
 const canvas = canvas.createCanvas(700, 250);
 client.once('ready', () => { 
