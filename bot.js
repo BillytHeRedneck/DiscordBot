e4ac4beea2b1fee70e68eb27b4dacd54ede0432e
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-03-18 12:26:06 -0400

    mute

--- bot.js
+++ bot.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js');
 const{ prefix, prefix2, token } = require ('./config.json');
 const client = new Discord.Client();
-const colours = require("../colours.json");
-const superagent = require("superagent");
+
 
 
 client.once('ready', () => { 
@@ -112,9 +111,15 @@ client.on('message', message=> {
         var counter = 0;
         if (willIWork == 0){
             message.channel.send("These controls seem to be damaged! Have no fear, I’m sure I can do it! Arrgh...this isn't working! Please give me some time to reboot")
-            
-                    
-         
+            function sleep(milliseconds) {
+                var start = new Date().getTime();
+                for (var i = 0; i < 1e7; i++) {
+                    if ((new Date().getTime() - start) > milliseconds) {
+                        break;
+                    }
+                    }
+            }
+            sleep(10000)             
         } else {
             for (var i = 1; i <= num; i++){
                 var rand = Math.floor(Math.random() * Math.floor(2));
@@ -135,75 +140,7 @@ client.on('message', message=> {
 
             }
         }
-    } else if (msg.startsWith(prefix+"mute")) {
-        let messageArray = message.content.split(" ")
-        let args1 = messageArray.slice(1);
-        module.exports.run = async (client, message, args1) => {
-            // check if the command caller has permission to use the command
-            if(!message.member.hasPermission("MANAGE_ROLES") || !message.guild.owner) return message.channel.send("You dont have permission to use this command.");
-            
-            if(!message.guild.me.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"])) return message.channel.send("I don't have permission to add roles!")
-            
-            //define the reason and mutee
-            let mutee = message.mentions.members.first() || message.guild.members.get(args[0]);
-            if(!mutee) return message.channel.send("Please supply a user to be muted!");
-            
-            let reason = args1.slice(1).join(" ");
-            if(!reason) reason = "No reason given"
-            
-            //define mute role and if the mute role doesnt exist then create one
-            let muterole = message.guild.roles.find(r => r.name === "Muted")
-            if(!muterole) {
-                try{
-                    muterole = await message.guild.createRole({
-                        name: "Muted",
-                        color: "#514f48",
-                        permissions: []
-                    })
-                    message.guild.channels.forEach(async (channel, id) => {
-                        await channel.overwritePermissions(muterole, {
-                            SEND_MESSAGES: false,
-                            ADD_REACTIONS: false,
-                            SEND_TTS_MESSAGES: false,
-                            ATTACH_FILES: false,
-                            SPEAK: false
-                        })
-                    })
-                } catch(e) {
-                    console.log(e.stack);
-                }
-            }
-        
-            
-            //add role to the mentioned user and also send the user a dm explaing where and why they were muted
-            mutee.addRole(muterole.id).then(() => {
-                message.delete()
-                mutee.send(`Hello, you have been in ${message.guild.name} for: ${reason}`).catch(err => console.log(err))
-                message.channel.send(`${mutee.user.username} was successfully muted.`)
-            })
-            
-            //send an embed to the modlogs channel
-            let embed = new Discord.RichEmbed()
-            .setColor(colours.redlight)
-            .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
-            .addField("Moderation:", "mute")
-            .addField("Mutee:", mutee.user.username)
-            .addField("Moderator:", message.author.username)
-            .addField("Reason:", reason)
-            .addField("Date:", message.createdAt.toLocaleString())
-            
-            let sChannel = message.guild.channels.find(c => c.name === "tut-modlogs")
-            sChannel.send(embed)
-            }
-            
-            module.exports.config = {
-                name: "mute",
-                description: "Mutes a member in the discord!",
-                usage: "!mute <user> <reason>",
-                accessableby: "Members",
-                aliases: ["m", "nospeak"]
-            }
-    }
+    } 
  
 })
 
