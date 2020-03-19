bcbc68de4e1d44fd6429eb1c9ccb69e075f1fdbe
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-03-19 18:03:22 -0400

    channel timed message

--- bot.js
+++ bot.js
@@ -16,15 +16,16 @@ client.once('ready', () => {
 })
 
 client.on('ready', () => {
-    const j = schedule.scheduleJob('0 17 ? * 0,4-6', function(){
+    
+    const j = schedule.scheduleJob('8 * * * *', function(){
         //if(dailyMeme==0) {
         const channel = client.channels.cache.get('688827517913530565');
         channel.send('The answer to life, the universe, and everything!');
         dailyMeme++
         //}
       });
-      const channel = client.channels.cache.get('688827517913530565');  
-      channel.send(j)
+      
+      
 
     /*
     setInterval(function()  {
