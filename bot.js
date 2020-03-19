24adf4c00d82c027ed76f69e4787af5b0d392071
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-03-19 15:28:23 -0400

    channel timed message

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ client.on('message', message=> {
     else if(msg.startsWith(prefix + "help")) {
         message.channel.send("Greetings Traveller! There are new gif-commands available at the Fyrestone Bounty-board!")
         message.channel.send(dailyMeme)
-        message.channel.send(moment().tz("America/New_York").format("dd HH:mm"))
+        message.channel.send(moment().tz("America/New_York").format("ddd HH:mm"))
         
 
     //GIF
