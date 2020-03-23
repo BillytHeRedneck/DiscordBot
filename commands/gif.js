366119db12cdf3f89fc21f75ad592451705c6ff0
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-03-23 15:47:28 -0400

    command handler basic

--- commands/gif.js
+++ commands/gif.js
@@ -6,5 +6,14 @@ module.exports = class gif {
     }
     run(client,message,ight){
         message.channel.send("I will now send a gif somehow and ight[1]: " + ight[1])
+        switch (ight[1]){
+            case 'eric':
+                message.channel.send({ files: ["./Images/Eric Andre Unimpressed.gif"] })
+                break
+            case 'yesss':
+                message.channel.send({ files: ["./Images/Yesss.gif"] })
+                break
+            
+        }
     }
 }
