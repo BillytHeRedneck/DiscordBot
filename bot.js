a5e664417fc27f14e75809d72bd27de0da632151
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-03-22 21:16:37 -0400

    sunday

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('ready', () => {
     var dailyMemeReminderWeekend = schedule.scheduleJob("0 1 * * 0,6", function () {    
             channel.send("Sure is lonely arou - Wrong day. Carry on.")  
     })
-    var dailyMemeReminderWeekend = schedule.scheduleJob("15 1 * * 0", function () {    
+    var dailyMemeReminderWeekend = schedule.scheduleJob("19 1 * * 1", function () {    
         channel1.send("Sure is lonely arou - Wrong day. Carry on.")  
 })
     
