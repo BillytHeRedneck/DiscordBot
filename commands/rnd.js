5c8d2ddd034e526f3ff5ad9b8c2bfc3bd68e432e
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-03-24 14:07:04 -0400

    command handler

---
+++ commands/rnd.js
@@ -0,0 +1,16 @@
+module.exports = class ht {
+    constructor() {
+        this.name = 'rnd'
+        this.alias = ['r']
+        this.usage = '!rnd'
+    }
+    run(message,ight) { 
+        var num = ight[1]
+        var willIWork = Math.floor(Math.random() * Math.floor(2));
+        if (willIWork == 0) {
+            message.channel.send("These controls seem to be damaged! Have no fear, I’m sure I can do it! Arrgh...this isn't working!")
+        } else {
+            message.channel.send("Your random number is... " + Math.floor(Math.random() * Math.floor(num)))
+        }
+    }
+}
---
+++ commands/time.js
@@ -0,0 +1,11 @@
+
+module.exports = class ht {
+    constructor() {
+        this.name = 'time'
+        this.alias = ['t']
+        this.usage = '!time'
+    }
+    run(message,ight) { 
+        message.channel.send("Hello Traveler. The time is " + moment().tz("America/New_York").format("h:mm A"))
+    }
+}
