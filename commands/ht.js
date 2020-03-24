ebc567a037fff107d42b85b21d88be9f7e2c1a7e
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-03-23 20:05:31 -0400

    command handler basic

--- commands/ht.js
+++ commands/ht.js
@@ -1,37 +1,39 @@
 module.exports = class ht {
-    constructor(){
+    constructor() {
         this.name = 'ht'
         this.alias = ['h']
         this.usage = '!ht'
     }
-    run(client,message,ight){
-        var willIWork = Math.floor(Math.random() * Math.floor(2));
-        var counter = 0;
-        var num = ight[1]
-        if (willIWork == 0) {
-            message.channel.send("These controls seem to be damaged! Have no fear, I’m sure I can do it! Arrgh...this isn't working!")
-            
-        } else {
-            for (var i = 1; i <= num; i++) {
-                var rand = Math.floor(Math.random() * Math.floor(2));
-                if (rand == 0) {
-                    counter++;
-                } else {
-                    counter--;
-                }
-            }
-            message.channel.send("On " + num + " rolls, the winner is... ")
-            if (counter > 0) {
-                message.channel.send({ files: ["./Images/Psycho Head.jpg"] })
-                
-            } else if (counter < 0) {
-                message.channel.send({ files: ["./Images/Skag Tail.jpg"] })
-                
+    run(client, message, ight) {
+        while (true) {
+            var willIWork = Math.floor(Math.random() * Math.floor(2));
+            var counter = 0;
+            var num = ight[1]
+            if (willIWork == 0) {
+                message.channel.send("These controls seem to be damaged! Have no fear, I’m sure I can do it! Arrgh...this isn't working!")
+                break
             } else {
-                message.channel.send("No one... what's this? I'm detecting a motor unit malfunction... I can't move! I'm paralyzed with fear!")
-                message.channel.send({ files: ["./Images/CL4P-TP Neutral.jpg"] })
-                
+                for (var i = 1; i <= num; i++) {
+                    var rand = Math.floor(Math.random() * Math.floor(2));
+                    if (rand == 0) {
+                        counter++;
+                    } else {
+                        counter--;
+                    }
+                }
+                message.channel.send("On " + num + " rolls, the winner is... ")
+                if (counter > 0) {
+                    message.channel.send({ files: ["./Images/Psycho Head.jpg"] })
+                    break
+                } else if (counter < 0) {
+                    message.channel.send({ files: ["./Images/Skag Tail.jpg"] })
+                    break
+                } else {
+                    message.channel.send("No one... what's this? I'm detecting a motor unit malfunction... I can't move! I'm paralyzed with fear!")
+                    message.channel.send({ files: ["./Images/CL4P-TP Neutral.jpg"] })
+                    break
 
+                }
             }
         }
     }
