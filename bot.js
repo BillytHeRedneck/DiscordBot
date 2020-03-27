93631c1d107c456c27ac43f710a66230780705a9
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-03-26 21:43:26 -0400

    unbox fix

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ const CH = new CommandHandler({
 })
 let meme = 0;
 let dailyMeme = 0
-global.lastBoy 
+global.lastBoy = null
 const client = new Client();
 client.commands = new Discord.Collection();
 
