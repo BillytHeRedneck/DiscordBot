4d8adffb80297966b20d00009ca47b6e50c6853c
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-03-27 20:33:13 -0400

    fix meme-a-day

--- bot.js
+++ bot.js
@@ -44,7 +44,7 @@ client.on('ready', () => {
     var dailyMemeReminderWeekend = schedule.scheduleJob("0 1 * * 6", function () {    
             channel.send("Sure is lonely arou - Wrong day. Carry on.")  
     })
-    var dailyMemeReminderWeekend1 = schedule.scheduleJob("33 0 * * 5", function () {    
+    var dailyMemeReminderWeekend1 = schedule.scheduleJob("35 20 * * 5", function () {    
         if (dailyMeme == 0){
         channel1.send("Day 5 - Friday")  
         } else {
