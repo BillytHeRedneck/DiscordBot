d6a8aa488caf5f8f0591e56218c2aff6eb6ffb64
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-07-21 00:46:08 -0400

    responds with message as a caption

--- commands/gif.js
+++ commands/gif.js
@@ -7,106 +7,85 @@ module.exports = class gif {
     }
     //run(client,message,ight){
     run(message,ight) { 
-        //this.deleteTheMessage(message)
         switch (ight[1]){
             case 'eric':
                 
-                message.channel.send({ files: ["./Images/Eric Andre Unimpressed.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/Eric Andre Unimpressed.gif"] })
                 break
             case 'yesss':
-                message.channel.send({ files: ["./Images/Yesss.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/Yesss.gif"] })
                 break
             case 'enjoythat':
-                message.channel.send({ files: ["./Images/EnjoyThat.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/EnjoyThat.gif"] })
                 break
             case 'nooice':
-                message.channel.send({ files: ["./Images/NOOICE.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/NOOICE.gif"] })
                 break
             case 'ora':
-                message.channel.send({ files: ["./Images/CatchHands.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/CatchHands.gif"] })
                 break
             case 'wrong':
-                message.channel.send({ files: ["./Images/Wrong.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/Wrong.gif"] })
                 break
             case 'fistbump':
                 message.channel.send(this.deleteTheMessage(message), { files: ["./Images/Fa-la-la-la-la.gif"] })
-                //message.channel.send({ files: ["./Images/Fa-la-la-la-la.gif"] })
                 break
             case 'idk':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/IDK.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/IDK.gif"] })
                 break
             case 'lblaugh':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/Long Beach Laugh.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/Long Beach Laugh.gif"] })
                 break
             case 'yesyes':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/M Bison Yes! Yes!.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/M Bison Yes! Yes!.gif"] })
                 break
             case 'spedcomp':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/Nice Competetion.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/Nice Competetion.gif"] })
                 break
             case 'tea':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/The Tea.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/The Tea.gif"] })
                 break
             case 'window':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/Throw You Out The Window.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/Throw You Out The Window.gif"] })
                 break
             case 'whathesay':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/What Did He Just Say.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/What Did He Just Say.gif"] })
                 break
             case 'neverwin':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/You Never Win.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/You Never Win.gif"] })
                 break
             case 'hurtmyear':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/You're Hurting My Ear.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/You're Hurting My Ear.gif"] })
                 break
             case 'highestfive':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/Highest of Fives.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/Highest of Fives.gif"] })
                 break
             case 'bigshaqno':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/Big Shaq No.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/Big Shaq No.gif"] })
                 break
             case 'bigshaqok':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/Big Shaq Ok.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/Big Shaq Ok.gif"] })
                 break
             case 'garretsmh':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/Garret Shake Head.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/Garret Shake Head.gif"] })
                 break
             case 'amazing':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/Squidward Amazing.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/Squidward Amazing.gif"] })
                 break
             case 'sweating':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/Sweating.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/Sweating.gif"] })
                 break
             case 't1cringe':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/Tyler1 Unimpressed.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/Tyler1 Unimpressed.gif"] })
                 break
             case 'devilno':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/Yeah...no.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/Yeah...no.gif"] })
                 break
             case 'heheboy':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/HeheBoy.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/HeheBoy.gif"] })
                 break
             case 'crunk':
-                this.deleteTheMessage(message)
-                message.channel.send({ files: ["./Images/CrunkAintDead.gif"] })
+                message.channel.send(this.deleteTheMessage(message), { files: ["./Images/CrunkAintDead.gif"] })
                 break
             default:
                 message.channel.send("I don't know that one. You either spelled it wrong" +
@@ -119,7 +98,6 @@ module.exports = class gif {
         let messageAuthor = message.member.displayName
         message.delete(message);
         return messageAuthor + " responds with: "
-        //message.channel.send(messageAuthor + " responds with: ")
     }
 }
 /*
