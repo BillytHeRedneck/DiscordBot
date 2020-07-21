314170c5ce9299ecd10b5371af620c2fe2a7f793
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-07-21 13:28:56 -0400

    startgame command

--- commands/startgame.js
+++ commands/startgame.js
@@ -7,6 +7,8 @@ module.exports = class gif {
     
     run(message,ight) { 
         message.channel.send(
+            "You will be X, I will be O. Refer to the game directions if Umar hasn't told you how to play\n" +
+            "I will be nice and let you go first\n"+
             "*1*  |  *2*  |  *3*\n" +
             "*4*  |  *5*  |  *6*\n" +
             "*7*  |  *8*  |  *9*\n"
