df4d65295ba8f0c5c267496bb3275b998853cff6
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-07-25 21:50:10 -0400

    revive CLAPTRAP

--- commands/gif.js
+++ commands/gif.js
@@ -5,7 +5,7 @@ module.exports = class gif {
         this.alias = ['g']
         this.usage = '!gif'
     }
-    //run(client,message,ight)hino{
+    //run(client,message,ight)hinoyes{
     run(message,ight) { 
         switch (ight[1]){
             case 'eric':
