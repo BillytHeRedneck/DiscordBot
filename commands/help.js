33679ff8cd0dae59482f02a61a693750ff96c537
Muhammad Umar <Umar@Muhammads-MacBook-Pro.local>
2020-07-27 15:42:24 -0400

    fixing the gif responds with

--- commands/help.js
+++ commands/help.js
@@ -8,16 +8,17 @@ module.exports = class time {
         message.channel.send("Allow me to introduce myself! I am a CL4P-TP steward bot," + 
         " but my friends call me Claptrap! Or they would, if any of them were still alive. Or" +
         " had existed in the first place! Oh -- I've got something for you!")
+        message.channel.send("*humming*")
         setTimeout(function() {
             message.channel.send(
-            "**!gif GIF_NAME** will give you a gif\n" +
+            "**!g GIF_NAME** will give you a gif\n" +
             "**!unbox** will give you one of The Boys to do the unboxing\n" +
-            "**!setboy NAME** will set the last unboxer to whatever NAME is\n" +
-            "**!getboy** will give you the last unboxer\n" +
+            "**!sb NAME** will set the last unboxer to whatever NAME is\n" +
+            "**!gb** will give you the last unboxer\n" +
             "**!time** will give you you the time\n" +
             "**!ht NUM** will flip a coin NUM amount of times and tell you the winner\n" +
             "**!rnd NUM** will give you a random number between 1 and NUM\n" +
-            "**!dice** will roll a dice and give you the result\n")
+            "**!d** will roll a dice and give you the result\n")
         }, 5000)
         
     }
